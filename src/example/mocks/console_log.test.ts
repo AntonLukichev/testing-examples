1304e1910a43f1079931ecf825a7d6cd47201ebd
Anton Lukichev <anton@lukichev.pro>
2020-07-01 12:38:14 +0300

    up console.log

--- src/example/mocks/console_log.test.ts
+++ src/example/mocks/console_log.test.ts
@@ -2,18 +2,35 @@
 import * as React from 'react';
 
 describe('Console.log mock', () => {
+  /* eslint-disable no-console */
   let consoleOutput: never[] = [];
   const mockConsoleLog = (...output: never) => consoleOutput.push(output);
 
   beforeEach(() => {
     consoleOutput = [];
-    // eslint-disable-next-line no-console
     console.log = mockConsoleLog;
   });
 
   it('should match output', () => {
-    // eslint-disable-next-line no-console
     console.log('test message', 123);
     expect(consoleOutput).toEqual([['test message', 123]]);
+  });
+  /* eslint-enable no-console */
+});
+
+describe('Console spyOn', () => {
+  it('should intercept warnings', () => {
+    /* eslint-disable no-console */
+    const warning = jest.spyOn(console, 'warn').mockImplementation();
+    console.warn('one');
+    console.warn('two');
+    expect(warning).toHaveBeenCalledTimes(2);
+    expect(warning).toHaveBeenLastCalledWith('two');
+    expect(warning.mock.calls).toEqual([['one'], ['two']]);
+    warning.mockRestore();
+
+    console.warn('three');
+    expect(warning).toHaveBeenCalledTimes(0);
+    /* eslint-enable no-console */
   });
 });
